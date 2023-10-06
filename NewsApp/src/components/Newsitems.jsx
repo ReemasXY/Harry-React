7b8c6be24fe5bb86edf1ea93c6cd1a7893d60ed4
ReemasXY <sameermhr345@gmail.com>
2023-10-06 16:59:38 +0545

    commit

--- NewsApp/src/components/Newsitems.jsx
+++ NewsApp/src/components/Newsitems.jsx
@@ -2,22 +2,25 @@ import React, { Component } from "react";
 
 export default class Newsitems extends Component {
   render() {
-    let { title, description } = this.props;
+    let { title, description, imageurl } = this.props;
+
     return (
       <>
-        <div className="col-md-4">
-          <div className="card">
-            <img src="..." className="card-img-top" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title">{title}</h5>
-              <p className="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-              <a href="#" className="btn btn-primary">
-                Go somewhere
-              </a>
-            </div>
+        <div className="card h-100">
+          <img
+            src={imageurl}
+            className="card-img-top"
+            alt="..."
+            style={{ height: "300px", objectFit: "cover" }}
+          />
+          <div className="card-body">
+            <h5 className="card-title">{title + "..."}</h5>
+            <p className="card-text">{description}</p>
+          </div>
+          <div className="card-footer border-0 bg-white">
+            <a href="#" className="btn btn-primary">
+              Read More
+            </a>
           </div>
         </div>
       </>
